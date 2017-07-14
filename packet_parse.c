e7c6b583938497a801df2fe6aaccf4a95cfe99f7
1nzag <wkdwns420@gmail.com>
2017-07-14 12:08:52 -0400

    Add packet parser header file

---
+++ packet_parse.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "packet_parse.h"
+
+
+void parse_ethernet(struct ether_header* eth_header)
+{
+	int i;
+	unsigned char addr;
+	printf("* ETHERNET INFOMATION\n");
+	printf("SOURCE MAC: ");
+	for(i = 0; i < 6; i++)
+	{
+		addr = (char)(eth_header->ether_shost[i]);
+		printf("%02x",addr);
+		if(i == 5)
+		{
+			printf("\n");
+			break;
+		}
+		printf(":");
+	}
+	printf("DESTINATION: ");
+	for(i = 0; i<6; i++)
+	{
+		addr = (char)(eth_header->ether_dhost[i]);
+		printf("%02x",addr);
+		if(i == 5)
+		{
+			printf("\n");
+			break;
+		}
+		printf(":");
+	}
+}
+
+void parse_ip(struct ip* ip_header)
+{
+	int i;
+	unsigned int addr;
+	printf("* IP INFOMATION\n");
+	printf("SOURCE IP: ");
+	addr = (int)(ip_header->ip_src).s_addr;
+	for(i=0;i<4;i++)
+	{
+		printf("%d", (addr >> (8 * (3 - i))) & 0xff);
+		if(i == 3) {printf("\n"); break;}
+		printf(".");
+	}
+	printf("DESTINATION IP: ");
+	addr = (int)(ip_header->ip_dst).s_addr;
+	for(i=0;i<4;i++)
+	{
+		printf("%d", (addr >> (8 * (3 - i))) & 0xff);
+		if(i == 3){printf("\n");break;}
+		printf(".");
+	}
+}
+
+void parse_tcp(struct tcphdr *tcp_header)
+{
+	printf("* TCP INFOMATION\n");
+	printf("SOURCE PORT: %u\n",(unsigned short)tcp_header->th_sport);
+	printf("DESTINATION PORT: %u\n",(unsigned short)tcp_header->th_dport);
+}
---
+++ packet_parse.h
@@ -0,0 +1,23 @@
+#include <pcap.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+
+#ifndef __parse_ethernet_h__
+#define __parse_ethernet_h__
+
+void parse_ethernet(struct ether_header* eth_header);
+#endif
+
+#ifndef __parse_ip_h__
+#define __parse_ip_h__
+
+void parse_ip(struct ip* ip_header);
+
+#endif
+
+#ifndef __parse_tcp_h__
+#define __parse_ecp_h__
+
+void parse_tcp(struct tcphdr *tcp_header);
+#endif

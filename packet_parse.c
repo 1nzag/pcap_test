e869fd7ad25f447c81d64fe45330b5d6b9de83a9
1nzag <wkdwns420@gmail.com>
2017-07-23 13:15:56 -0400

    Enhance inet_ntop

--- packet_parse.c
+++ packet_parse.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "packet_parse.h"
 
-
 void parse_ethernet(struct ether_header* eth_header)
 {
 	int i;
-	unsigned char addr;
+	uint8_t addr;
 	printf("* ETHERNET INFOMATION\n");
 	printf("SOURCE MAC: ");
 	for(i = 0; i < 6; i++)
@@ -36,29 +35,21 @@ void parse_ethernet(struct ether_header* eth_header)
 void parse_ip(struct ip* ip_header)
 {
 	int i;
-	unsigned int addr;
+	uint32_t addr;
+	char src_addr[INET_ADDRSTRLEN] = {0,};
+	char dest_addr[INET_ADDRSTRLEN] = {0,};
+
 	printf("* IP INFOMATION\n");
-	printf("SOURCE IP: ");
 	addr = (int)(ip_header->ip_src).s_addr;
-	for(i=0;i<4;i++)
-	{
-		printf("%d", (addr >> (8 * (3 - i))) & 0xff);
-		if(i == 3) {printf("\n"); break;}
-		printf(".");
-	}
-	printf("DESTINATION IP: ");
-	addr = (int)(ip_header->ip_dst).s_addr;
-	for(i=0;i<4;i++)
-	{
-		printf("%d", (addr >> (8 * (3 - i))) & 0xff);
-		if(i == 3){printf("\n");break;}
-		printf(".");
-	}
+	inet_ntop(AF_INET,&(ip_header->ip_src).s_addr,src_addr,sizeof(src_addr));
+	inet_ntop(AF_INET,&(ip_header->ip_dst).s_addr,dest_addr,sizeof(dest_addr));
+	printf("SOURCE IP: %s\n",src_addr);
+	printf("DESTINATION IP: %s\n",dest_addr);
 }
 
 void parse_tcp(struct tcphdr *tcp_header)
 {
 	printf("* TCP INFOMATION\n");
-	printf("SOURCE PORT: %u\n",(unsigned short)tcp_header->th_sport);
-	printf("DESTINATION PORT: %u\n",(unsigned short)tcp_header->th_dport);
+	printf("SOURCE PORT: %d\n",ntohs(tcp_header->source));
+	printf("DESTINATION PORT: %d\n",ntohs(tcp_header->dest));
 }
